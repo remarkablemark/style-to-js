30d6a0ab879149ba8f8135544121db1b536b40ef
Mark <mark@remarkablemark.org>
2020-11-22 16:11:13 -0500

    refactor(utilities): modularize functions from `camelCase`

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,19 +4,33 @@ const NO_HYPHEN_REGEX = /^[^-]+$/;
 const VENDOR_PREFIX_REGEX = /^-(webkit|moz|ms|o)-/;
 
 /**
+ * Checks whether to skip camelCase.
+ */
+const skipCamelCase = (property: string) =>
+  !property ||
+  NO_HYPHEN_REGEX.test(property) ||
+  CUSTOM_PROPERTY_REGEX.test(property);
+
+/**
+ * Replacer that capitalizes first character.
+ */
+const capitalize = (match: string, character: string) =>
+  character.toUpperCase();
+
+/**
+ * Replacer that removes beginning hyphen of vendor prefix property.
+ */
+const trimHyphen = (match: string, prefix: string) => `${prefix}-`;
+
+/**
  * CamelCases a CSS property.
  */
 export const camelCase = (property: string) => {
-  if (
-    !property ||
-    NO_HYPHEN_REGEX.test(property) ||
-    CUSTOM_PROPERTY_REGEX.test(property)
-  ) {
+  if (skipCamelCase(property)) {
     return property;
   }
 
-  return property
-    .toLowerCase()
-    .replace(VENDOR_PREFIX_REGEX, (_, prefix) => `${prefix}-`)
-    .replace(HYPHEN_REGEX, (_, character) => character.toUpperCase());
+  property = property.toLowerCase();
+  property = property.replace(VENDOR_PREFIX_REGEX, trimHyphen);
+  return property.replace(HYPHEN_REGEX, capitalize);
 };
